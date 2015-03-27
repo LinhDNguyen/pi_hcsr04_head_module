18b50265755210ef818466abdc19f00c30e8b461
Linh Nguyen <B51960@freescale.com>
2015-03-27 10:23:31 +0700

    Update readme file.

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
-# pi_hcsr04_head_module
+pi_hcsr04_head_module
+================
 
 This driver create an character device which allows user configure the frequence
 of Untrasonic measurement.
 
+```
 - GPIO16 - J8 36: both HC-SR04's trigger pins
 - GPIO20 - J8 38:
 - GPIO21 - J8 40: these two pins are echo pins of two HC-SR04 sensor.
+```
+
+Hardware setups
+---------------
+
+The dual HC-SR04 sensors will use the same vcc, gnd and trigger pin signal.
+Look like bellow:
+```
+    +-----+                                                           +-----+
+    |     | gnd                  PI GND (J8-34)                   gnd |     |
+    |     |===========-----------------o-------------------===========|     |
+    |  H  |                                                           |  H  |
+    |  C  | echo     PI ECHO 1 (J8-38)     PI ECHO 2 (J8-40)     echo |  C  |
+    |  -  |===========----o                          o-----===========|  -  |
+    |  S  |                                                           |  S  |
+    |  R  | trigger          PI TRIGGER PIN (J8-36)           trigger |  R  |
+    |  0  |===========----------------o--------------------===========|  0  |
+    |  4  |                                                           |  4  |
+    |     | vcc                     PI 5V (J8-02)                 vcc |     |
+    |     |===========----------------o--------------------===========|     |
+    +-----+                                                           +-----+
+```
+
+Usage
+-----
+
+This module will create character device */dev/dual_hcsr04*, and you can write a
+number value to set sampling frequence, read go get latest distance value of
+both HC-SR04 sensor.
+
+### Sampling frequence
+
+You can set sampling frequence by write this number to */dev/dual_hcsr04*
+```bash
+echo 10 > /dev/dual_hcsr04
+```
+will set sampling frequence to 10 samples per second.
+Maximum frequence is 50 samples per second.
+If frequence is 0, the module will stop measurement.
+```bash
+echo 0 > /dev/dual_hcsr04
+```
+
+### Get distance value
+
+Read file */dev/dual_hcsr04* to get latest value of both HC-SR04 sensors.
+```bash
+cat /dev/dual_hcsr04
+```
--- gpiomod_dual_hcsr04.c
+++ gpiomod_dual_hcsr04.c
@@ -15,11 +15,11 @@
 #include <linux/init.h>
 
 /* Timer struct, used to create an priodic timer */
-static struct timer_list scan_timer;
+static struct timer_list schedule_timer;
 
 /* Define GPIOs for trigger pin */
 static struct gpio triggers[] = {
-        {  16, GPIOF_OUT_INIT_LOW, "trigger" },
+        {  16, GPIOF_OUT_INIT_LOW, "Trigger" },
 };
 
 /* Define GPIOs for echo pins */
@@ -28,19 +28,28 @@ static struct gpio echos[] = {
         { 21, GPIOF_IN, "Echo 2" },
 };
 
-/* Later on, the assigned IRQ numbers for the buttons are stored here */
-static int button_irqs[] = { -1, -1 };
+/* Later on, the assigned IRQ numbers for the echos are stored here */
+static int echo_irqs[] = { -1, -1 };
+
+/* Frequence of sampling */
+static uint sampl_frequence = 0;
+
+/* Latest distance value */
 
 /*
- * The interrupt service routine called on button presses
+ * The interrupt service routine called on echo signal start/end
  */
-static irqreturn_t button_isr(int irq, void *data)
+static irqreturn_t echo_isr(int irq, void *data)
 {
-    if(irq == button_irqs[0] && !gpio_get_value(leds[0].gpio)) {
-            gpio_set_value(leds[0].gpio, 1);
+    if(irq == echo_irqs[0] && gpio_get_value(echos[0].gpio)) {
+        // Echo 1 started
+    } else if(irq == echo_irqs[0] && !gpio_get_value(echos[0].gpio)) {
+        // Echo 1 ended
     }
-    else if(irq == button_irqs[1] && gpio_get_value(leds[0].gpio)) {
-            gpio_set_value(leds[0].gpio, 0);
+    if(irq == echo_irqs[1] && gpio_get_value(echos[1].gpio)) {
+        // Echo 2 started
+    } else if(irq == echo_irqs[1] && !gpio_get_value(echos[1].gpio)) {
+        // Echo 2 ended
     }
 
     return IRQ_HANDLED;
@@ -55,36 +64,36 @@ static int __init gpiomode_init(void)
 
     printk(KERN_INFO "%s\n", __func__);
 
-    // register LED gpios
-    ret = gpio_request_array(leds, ARRAY_SIZE(leds));
+    // register trigger pin
+    ret = gpio_request_array(triggers, ARRAY_SIZE(triggers));
 
     if (ret) {
-        printk(KERN_ERR "Unable to request GPIOs for LEDs: %d\n", ret);
+        printk(KERN_ERR "Unable to request GPIOs for triggers: %d\n", ret);
         return ret;
     }
 
-    // register BUTTON gpios
-    ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
+    // register ECHO gpios
+    ret = gpio_request_array(echos, ARRAY_SIZE(echos));
 
     if (ret) {
-        printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+        printk(KERN_ERR "Unable to request GPIOs for echos: %d\n", ret);
         goto fail1;
     }
 
-    printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
+    printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(echos[0].gpio));
 
-    ret = gpio_to_irq(buttons[0].gpio);
+    ret = gpio_to_irq(echos[0].gpio);
 
     if(ret < 0) {
         printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
         goto fail2;
     }
 
-    button_irqs[0] = ret;
+    echo_irqs[0] = ret;
 
-    printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
+    printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", echo_irqs[0]);
 
-    ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button1", NULL);
+    ret = request_irq(echo_irqs[0], echo_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button1", NULL);
 
     if(ret) {
         printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
@@ -92,35 +101,37 @@ static int __init gpiomode_init(void)
     }
 
 
-    ret = gpio_to_irq(buttons[1].gpio);
+    ret = gpio_to_irq(echos[1].gpio);
 
     if(ret < 0) {
         printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
         goto fail2;
     }
 
-    button_irqs[1] = ret;
+    echo_irqs[1] = ret;
 
-    printk(KERN_INFO "Successfully requested BUTTON2 IRQ # %d\n", button_irqs[1]);
+    printk(KERN_INFO "Successfully requested BUTTON2 IRQ # %d\n", echo_irqs[1]);
 
-    ret = request_irq(button_irqs[1], button_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button2", NULL);
+    ret = request_irq(echo_irqs[1], echo_isr, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#button2", NULL);
 
     if(ret) {
         printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
         goto fail3;
     }
 
+    /* Initialize timer for scheduling */
+
     return 0;
 
 // cleanup what has been setup so far
 fail3:
-    free_irq(button_irqs[0], NULL);
+    free_irq(echo_irqs[0], NULL);
 
 fail2:
-    gpio_free_array(buttons, ARRAY_SIZE(leds));
+    gpio_free_array(echos, ARRAY_SIZE(triggers));
 
 fail1:
-    gpio_free_array(leds, ARRAY_SIZE(leds));
+    gpio_free_array(triggers, ARRAY_SIZE(triggers));
 
     return ret;
 }
@@ -135,22 +146,22 @@ static void __exit gpiomode_exit(void)
     printk(KERN_INFO "%s\n", __func__);
 
     // free irqs
-    free_irq(button_irqs[0], NULL);
-    free_irq(button_irqs[1], NULL);
+    free_irq(echo_irqs[0], NULL);
+    free_irq(echo_irqs[1], NULL);
 
-    // turn all LEDs off
-    for(i = 0; i < ARRAY_SIZE(leds); i++) {
-        gpio_set_value(leds[i].gpio, 0);
+    // turn all triggers off
+    for(i = 0; i < ARRAY_SIZE(triggers); i++) {
+        gpio_set_value(triggers[i].gpio, 0);
     }
 
     // unregister
-    gpio_free_array(leds, ARRAY_SIZE(leds));
-    gpio_free_array(buttons, ARRAY_SIZE(buttons));
+    gpio_free_array(triggers, ARRAY_SIZE(triggers));
+    gpio_free_array(echos, ARRAY_SIZE(echos));
 }
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Stefan Wendler");
-MODULE_DESCRIPTION("Basic Linux Kernel module using GPIO interrupts");
+MODULE_LICENSE("BSD");
+MODULE_AUTHOR("Linh Nguyen");
+MODULE_DESCRIPTION("PI B+ kernel module for measuring distance using dual HC-SR04");
 
 module_init(gpiomode_init);
 module_exit(gpiomode_exit);
